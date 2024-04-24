7eccbdb93983580626301cd4985e099adea93521
mlorenzoo <malool@fp.insjoaquimmir.cat>
2024-04-24 16:59:18 +0200

    Laravel changed to 10.0

--- vendor/phpunit/phpunit/src/Util/Cloner.php
+++ vendor/phpunit/phpunit/src/Util/Cloner.php
@@ -17,7 +17,7 @@ use Throwable;
 final class Cloner
 {
     /**
-     * @psalm-template OriginalType
+     * @psalm-template OriginalType of object
      *
      * @psalm-param OriginalType $original
      *
@@ -27,7 +27,7 @@ final class Cloner
     {
         try {
             return clone $original;
-        } catch (Throwable $t) {
+        } catch (Throwable) {
             return $original;
         }
     }
